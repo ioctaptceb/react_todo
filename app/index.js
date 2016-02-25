3f567a663f45c371ea37f4b195f01e5a1b4a7499
ioctaptceb <yulia.startsev@gmail.com>
2016-02-25 18:13:28 +0100

    flush out the ui and todo model
    everything is all about zombies

--- app/index.js
+++ app/index.js
@@ -1,8 +1,50 @@
-var message = React.createClass({
+var app = app || {};
 
-  render: function () {
-    return '<h1> HI GUYS </h1>'
+(function () {
+  app.ZombieModel = function() {
+    this.dudelings = [];
+    this.listeners = [];
   }
-});
 
-ReactDOM.render('<message>', document.body);
+  app.ZombieModel.prototype.subscribe = function (onChange) {
+    this.listeners.push(onChange);
+  }
+
+  app.ZombieModel.prototype.publish = function () {
+    this.listeners.forEach( function (callback) { callback(); });
+  }
+
+  app.ZombieModel.prototype.add = function (text) {
+    this.dudelings.push(new app.Zombie(text));
+    this.publish();
+  }
+
+  app.ZombieModel.prototype.remove = function (item) {
+    this.dudelings = this.dudelings.filter(function (zombie) { return item !== zombie.id });
+    this.publish();
+  }
+
+  app.Zombie = function(name) {
+    this.name = name;
+    this.id = Math.abs(Math.floor(Math.random() * (1 - 100 + 1)) + 1)
+    this.complete = false;
+    this.portrait = getPortrait();
+
+  }
+
+  var portraits = {};
+
+  function getPortrait() {
+    var id = Math.abs(Math.floor(Math.random() * (1 - 9 + 1)) + 1);
+
+    if (portraits && Object.keys(portraits).length === 9) { portraits = {} };
+
+    if (portraits[id]) { return getPortrait(); }
+
+    portraits[id] = id;
+
+    return 'portrait-skulls portrait-skulls--' + id;
+  }
+
+})();
+
